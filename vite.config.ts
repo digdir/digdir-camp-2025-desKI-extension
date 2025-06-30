ae490b6242bd66b0d803df588b846a44e1ee8f96
magnus-lower <magnus.lower@gmail.com>
2025-06-30 10:03:08 +0200

    Included comments in the vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+/*
+    * Vite configuration file for the React application.
+ */
 export default defineConfig({
   plugins: [
     react(),
