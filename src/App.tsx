747745499be3b498717ce907e35f53979498882b
Brinje Marie Haugli <brinje.marie.haugli@digdir.no>
2025-07-14 09:48:57 +0200

    Feat: fix after test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<GridMenu />} />
+          <Route path="/" element={<StartPage />} />
           <Route path="/grid" element={<GridMenu />} />
           <Route path="/:slug" element={<ChatPage />} />
         </Routes>
