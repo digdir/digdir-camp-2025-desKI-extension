04d029e7159e3a87ab53035b9900cb590969aed9
Brinje Marie Haugli <brinje.marie.haugli@digdir.no>
2025-07-15 14:31:42 +0200

    Fix: removed test immplementation

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,12 +1,7 @@
-import { ChatBubble } from "../components/ChatBubble";
-import { DropDownMenu } from "../components/DropdownMenu";
-import { servicedeskSolutions } from "../data/servidedeskSolutions";
-
 function Chat() {
   return (
     <div>
       <h1>Welcome to the Chat</h1>
-      <DropDownMenu solutions={servicedeskSolutions} />
     </div>
   );
 }
--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,11 +1,7 @@
-import { DropDownMenu  } from "../components/DropdownMenu";
-import { brukerstøtteSolutions } from "../data/brukerstøtteSolutions";
-
 function StartPage() {
   return (
     <div>
       <h1>Welcome to the Start Menu</h1>
-      <DropDownMenu solutions={brukerstøtteSolutions} />
 
     </div>
   );
