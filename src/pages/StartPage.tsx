3aff8361cb6346790e77d3528f5555b4232ec414
Audun Kristian Oklevik <audun.oklevik@gmail.com>
2025-07-15 14:54:31 +0200

    UtilityBar optimizations

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,24 +1,12 @@
+import { UtilityBar } from "src/components/utilityBar";
 import { logoLight } from "../assets";
 
 export function StartPage() {
   return (
     <div className="min-h-screen flex flex-col items-center bg-[#f2f3f5] px-6 py-10">
 
-      {/* Top-right toggles */}
-      <div className="w-full flex justify-end mb-6">
-      <button
-        aria-label="Toggle dark mode"
-        className="p-2 rounded-md hover:bg-gray-200 transition"
-      >
-        {/* <Moon className="w-6 h-6 text-gray-700" /> */}
-      </button>
-      <button
-        aria-label="Change language"
-        className="p-2 rounded-md hover:bg-gray-200 transition"
-      >
-        {/* <Globe className="w-6 h-6 text-gray-700" /> */}
-      </button>
-    </div>
+      {/* Utility Bar */}
+      <UtilityBar />
 
       {/* Logo */}
       <img
