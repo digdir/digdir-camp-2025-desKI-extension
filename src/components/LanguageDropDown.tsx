da66a060c68d8588c4c4393f6fe27477b9b54d4e
Audun Kristian Oklevik <audun.oklevik@gmail.com>
2025-07-16 12:38:21 +0200

    Added file

---
+++ src/components/LanguageDropDown.tsx
@@ -0,0 +1,11 @@
+
+export function LanguageDropdown() {
+  return (
+    <select>
+      <option value="en">English</option>
+      <option value="no">Norsk</option>
+    </select>
+  );
+}
+
+export default LanguageDropdown;
