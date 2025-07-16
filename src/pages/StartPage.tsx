f60d3778907b226e9cb5353735d721811e5d2d7b
Audun Kristian Oklevik <audun.oklevik@gmail.com>
2025-07-16 15:37:29 +0200

    Added position

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -3,7 +3,7 @@ import { InputField } from "../components/InputField";
 
 function StartPage() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col items-center bg-[#f2f3f5] px-6 py-10">
       <Logo />
       <h1>Welcome to the Start Menu</h1>
     </div>
