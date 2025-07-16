2bac29f0e570372ce125ee52e721e88362b2a021
Audun Kristian Oklevik <audun.oklevik@gmail.com>
2025-07-16 15:34:28 +0200

    Added more changes

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,8 +1,10 @@
+import { Logo } from "../components/Logo";
 import { InputField } from "../components/InputField";
 
 function StartPage() {
   return (
     <div>
+      <Logo />
       <h1>Welcome to the Start Menu</h1>
     </div>
 
