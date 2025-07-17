84f9ef0bb900022ef574aa7cf4971920d051c297
AudunKristian <audun.oklevik@gmail.com>
2025-07-17 09:24:03 +0200

    15 add deski logo now working (#43)
    * added logo
    
    * Added logo
    
    * Added more changes
    
    * Added position

---
+++ src/components/Logo.tsx
@@ -0,0 +1,11 @@
+import {logoLight} from '../assets';
+
+export function Logo() {
+  return (
+    <img
+      src={logoLight}
+      alt="desKI logo"
+      className="mb-10 w-48 md:w-64 lg:w-72"
+    />
+  );
+}
