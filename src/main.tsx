ea85f229e408c94b58eb8101001d48176f280897
Brinje Marie Haugli <brinje.marie.haugli@digdir.no>
2025-07-17 09:10:15 +0200

    fix: switch to HashRouter for Chrome Extension compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import "./styles/common.css"
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 
 import App from '../src/App';
-import "../src/css/index.css";
+import '../src/css/index.css';
+import './styles/common.css';
 import '@digdir/designsystemet-theme';
 import '@digdir/designsystemet-css';
 
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 );
+
