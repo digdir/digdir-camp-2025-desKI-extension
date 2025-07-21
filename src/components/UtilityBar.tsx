4c7142e4ee92eecaa2b8667c7224eb3f0f017939
Audun Kristian Oklevik <audun.oklevik@gmail.com>
2025-07-21 10:51:23 +0200

    added lib

--- src/components/UtilityBar.tsx
+++ src/components/UtilityBar.tsx
@@ -1,15 +1,11 @@
 import { Button } from '@digdir/designsystemet-react';
 import { EarthIcon, MoonIcon } from '@navikt/aksel-icons';
+import ColorModeSwitch from './ColorModeSwitch';
 
 export function UtilityBar() {
   return (
     <div className="fixed top-2 right-2 z-[100] flex items-center gap-0.5">
-      <Button
-        aria-label="Switch to dark mode"
-        className="bg-transparent text-[color:var(--ds-color-main-text)] p-0 text-lg hover:bg-[color:var(--ds-color-neutral-surface-hover)] hover:text-[color:var(--ds-color-main-text)] focus:outline-none border-none outline-none"
-      >
-        <MoonIcon className="p-0 m-0" />
-      </Button>
+      <ColorModeSwitch />
       <Button
         aria-label="Choose language"
         className="bg-transparent text-[color:var(--ds-color-main-text)] p-0 text-lg hover:bg-[color:var(--ds-color-neutral-surface-hover)] hover:text-[color:var(--ds-color-main-text)] focus:outline-none border-none outline-none"
