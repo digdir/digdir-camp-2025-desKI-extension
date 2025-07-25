03aed68d1dab80e8f0068b1e53a8cbf0c0c84fd0
Audun Kristian Oklevik <audun.oklevik@gmail.com>
2025-07-25 19:04:25 +0200

    Added darkmode stuff to avoid conflicts later. We will see if this works

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,30 @@
+import { Button, Tooltip } from "@digdir/designsystemet-react";
+import { SunIcon, MoonIcon } from '@navikt/aksel-icons';
+import { ColorMode, useColorMode } from '../lib/settings';
+
+
+export default function ColorModeSwitch() {
+  const [colorMode, setColorMode] = useColorMode();
+
+  // Fallback to system on first load — but only toggle light/dark manually
+  const toggleColorMode = () => {
+    const newMode = colorMode === 'dark' ? ColorMode.Light : ColorMode.Dark;
+    setColorMode(newMode);
+  };
+
+  const icon = colorMode === 'dark' ? <SunIcon /> : <MoonIcon />;
+  const tooltipText = colorMode === 'dark' ? "Switch to light mode" : "Switch to dark mode";
+
+
+  return (
+    <Tooltip content={tooltipText} placement="bottom">
+      <Button
+        variant="tertiary"
+        className="bg-transparent text-[color:var(--ds-color-main-text)] p-0 text-lg hover:bg-[color:var(--ds-color-neutral-surface-hover)] hover:text-[color:var(--ds-color-main-text)] focus:outline-none border-none outline-none"
+        onClick={toggleColorMode}
+      >
+        {icon}
+      </Button>
+    </Tooltip>
+  );
+};
