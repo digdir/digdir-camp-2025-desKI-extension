ce7a5e05a94e3355d1067d2abfff1116701bcd78
Audun Kristian Oklevik <audun.oklevik@gmail.com>
2025-07-25 18:54:54 +0200

    Added translation component and i18n folder

---
+++ src/i18n/constants.ts
@@ -0,0 +1,42 @@
+/**
+ * All translatable keys used in the app.
+ */
+export const KEY = {
+  chatbot_welcome: 'chatbot_welcome',
+  initial_welcome: 'initial_welcome',
+  language: 'language',
+
+  solutions_list: 'solutions_list',
+
+  // ==================== //
+  //      Chat area       //
+  // ==================== //
+  chat_placeholder: 'chat_placeholder',
+  send: 'send',
+  upload_image: 'upload_image',
+
+  // ==================== //
+  //    Error messages    //
+  // ==================== //
+  api_connection_error: 'api_connection_error',
+
+  // ==================== //
+  //      UtilityBar      //
+  // ==================== //
+  // Color mode
+  theme_toggle: 'theme-toggle',
+  theme_dark: 'theme_dark',
+  theme_light: 'theme_light',
+
+  // Language
+  language_switch: 'language_switch',
+
+  // ==================== //
+  //        Labels        //
+  // ==================== //
+  close_image: 'close_image',
+  display_image_fullscreen: 'display_image_fullscreen',
+  go_to_homepage: 'go_to_homepage',
+  remove_image: 'remove_image',
+  select_solution: 'select_solution',
+} as const;
---
+++ src/i18n/translations.ts
@@ -0,0 +1,224 @@
+import { KEY } from './constants';
+
+/**
+ * English translations
+ */
+export const en = {
+  [KEY.chatbot_welcome]: 'What would you like to know?',
+  [KEY.initial_welcome]: 'Hi, which solution can I help you with today?',
+
+  [KEY.solutions_list]: [
+    'ID-porten',
+    'Maskinporten',
+    'eFormidling',
+    'eSignering',
+    'Kontakt- og reservasjonsregisteret',
+    'Digital postkasse til Innbyggere',
+    'eInnsyn',
+    'ELMA',
+    'Ansattporten',
+    'Peppol eGovernment',
+    'Digital lommebok',
+    'Other',
+  ],
+
+  // ==================== //
+  //      Chat area       //
+  // ==================== //
+  [KEY.chat_placeholder]: 'Ask a question here',
+  [KEY.send]: 'Send',
+  [KEY.upload_image]: 'Upload image',
+
+  // ==================== //
+  //    Error messages    //
+  // ==================== //
+  [KEY.api_connection_error]:
+    'Sorry, something went wrong with the connection to the desKI API',
+
+  // ==================== //
+  //      UtilityBar      //
+  // ==================== //
+  // Color mode
+  [KEY.theme_toggle]: 'Switch to {{theme}} mode',
+  [KEY.theme_dark]: 'dark',
+  [KEY.theme_light]: 'light',
+
+  // Language
+  [KEY.language_switch]: 'Switch language',
+
+  // ==================== //
+  //        Labels        //
+  // ==================== //
+  [KEY.close_image]: 'Close image',
+  [KEY.display_image_fullscreen]: 'Display image fullscreen',
+  [KEY.go_to_homepage]: 'Go to homepage',
+  [KEY.remove_image]: 'Remove image',
+  [KEY.select_solution]: 'Select solution',
+};
+
+/**
+ * Norsk bokmål translations
+ */
+export const nb = {
+  [KEY.chatbot_welcome]: 'Hva lurer du på?',
+  [KEY.initial_welcome]: 'Hei, hvilken løsning vil du ha hjelp med i dag?',
+
+  [KEY.solutions_list]: [
+    'ID-porten',
+    'Maskinporten',
+    'eFormidling',
+    'eSignering',
+    'Kontakt- og reservasjonsregisteret',
+    'Digital postkasse til Innbyggere',
+    'eInnsyn',
+    'ELMA',
+    'Ansattporten',
+    'Peppol eGovernment',
+    'Digital lommebok',
+    'Annet',
+  ],
+
+  // ==================== //
+  //      Chat area       //
+  // ==================== //
+  [KEY.chat_placeholder]: 'Spør et spørsmål her',
+  [KEY.send]: 'Send',
+  [KEY.upload_image]: 'Last opp bilete',
+
+  // ==================== //
+  //    Error messages    //
+  // ==================== //
+  [KEY.api_connection_error]:
+    'Beklager, noe gikk galt med forbindelsen til desKI api-et',
+
+  // ==================== //
+  //      UtilityBar      //
+  // ==================== //
+  // Color mode
+  [KEY.theme_toggle]: 'Bytt til {{theme}} modus',
+  [KEY.theme_dark]: 'mørk',
+  [KEY.theme_light]: 'lys',
+
+  // Language
+  [KEY.language_switch]: 'Bytt språk',
+
+  // ==================== //
+  //        Labels        //
+  // ==================== //
+  [KEY.close_image]: 'Lukk bilete',
+  [KEY.display_image_fullscreen]: 'Vis bilde i fullskjerm',
+  [KEY.go_to_homepage]: 'Gå til forsiden',
+  [KEY.remove_image]: 'Fjern bilde',
+  [KEY.select_solution]: 'Velg løsning',
+};
+
+/**
+ * Norsk nynorsk translations
+ */
+export const nn = {
+  [KEY.chatbot_welcome]: 'Kva lurer du på?',
+  [KEY.initial_welcome]: 'Hei, kva løysing vil du ha hjelp med i dag?',
+
+  [KEY.solutions_list]: [
+    'ID-porten',
+    'Maskinporten',
+    'eFormidling',
+    'eSignering',
+    'Kontakt- og reservasjonsregisteret',
+    'Digital postkasse til Innbyggere',
+    'eInnsyn',
+    'ELMA',
+    'Ansattporten',
+    'Peppol eGovernment',
+    'Digital lommebok',
+    'Anna',
+  ],
+
+  // ==================== //
+  //      Chat area       //
+  // ==================== //
+  [KEY.chat_placeholder]: 'Spør eit spørsmål her',
+  [KEY.send]: 'Send',
+  [KEY.upload_image]: 'Last opp bilete',
+
+  // ==================== //
+  //    Error messages    //
+  // ==================== //
+  [KEY.api_connection_error]:
+    'Beklagar, noko gjekk gale med sambandet til desKI API-et',
+
+  // ==================== //
+  //      UtilityBar      //
+  // ==================== //
+  //Color mode
+  [KEY.theme_toggle]: 'Bytt til {{theme}} modus',
+  [KEY.theme_dark]: 'mørk',
+  [KEY.theme_light]: 'lys',
+
+  // Language
+  [KEY.language_switch]: 'Bytt språk',
+
+  // ==================== //
+  //        Labels        //
+  // ==================== //
+  [KEY.close_image]: 'Lukk bilete',
+  [KEY.display_image_fullscreen]: 'Vis bilete i fullskjerm',
+  [KEY.go_to_homepage]: 'Gå til framsida',
+  [KEY.remove_image]: 'Fjern bilete',
+  [KEY.select_solution]: 'Velg løysing',
+};
+
+/**
+ * Nothern Sami translations
+ */
+export const ns = {
+  [KEY.chatbot_welcome]: 'Man birra imaštalat?',
+  [KEY.initial_welcome]: 'Hei, makkár čovdosiin don háliidat veahki odne?',
+  [KEY.solutions_list]: [
+    'ID-porten',
+    'Maskinporten',
+    'eFormidling',
+    'eSignering',
+    'Kontakt- og reservasjonsregistera',
+    'Digital postkasse til Innbyggere',
+    'eInnsyn',
+    'ELMA',
+    'Ansattporten',
+    'Peppol eGovernment',
+    'Digital lommebok',
+    'eará',
+  ],
+
+  // ==================== //
+  //      Chat area       //
+  // ==================== //
+  [KEY.chat_placeholder]: 'Jeara gažaldaga dás',
+  [KEY.send]: 'Sádde',
+  [KEY.upload_image]: 'Sádde gova',
+
+  // ==================== //
+  //    Error messages    //
+  // ==================== //
+  [KEY.api_connection_error]:
+    'Ándagassii, juoga manai funet desKI api oktavuođain',
+
+  // ==================== //
+  //      UtilityBar      //
+  // ==================== //
+  // Color mode
+  [KEY.theme_toggle]: 'Rievdat {{theme}} dihtii',
+  [KEY.theme_dark]: 'čáhppes',
+  [KEY.theme_light]: 'čuovgat',
+
+  // Language
+  [KEY.language_switch]: 'Molsso giela',
+
+  // ==================== //
+  //        Labels        //
+  // ==================== //
+  [KEY.close_image]: 'Bija gova gitta',
+  [KEY.display_image_fullscreen]: 'Čájet gova dievasšearbmas',
+  [KEY.go_to_homepage]: 'Mana ovdasiidui',
+  [KEY.remove_image]: 'Váldde gova eret',
+  [KEY.select_solution]: 'Vállje čovdosa',
+};
